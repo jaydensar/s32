2e8c7173a5eb9483852bd585994331ba14bfca9b
Jayden <jayden@jaydensar.net>
2021-05-30 14:31:12 -0400

    fix: use correct env var

--- index.js
+++ index.js
@@ -47,5 +47,5 @@ app.put("/game", (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${process.env.port || 3000}`);
+  console.log(`Listening on port ${process.env.PORT || 3000}`);
 });
